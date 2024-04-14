a7ee7fa0ddbc374985db963b9f61206a72afd815
lupe0614 <lupejara0614@gmail.com>
2024-04-14 16:26:35 -0700

    added new tab

--- src/Components/Landing/projects/SimpleTab.jsx
+++ src/Components/Landing/projects/SimpleTab.jsx
@@ -17,15 +17,18 @@ function SimpleTab() {
       <div className='box mb-5'>
       <ul className="nav nav-tabs" id="myTab" role="tablist">
         <li className="nav-item" role="presentation">
-          <button className="nav-link active tab-text-color" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Software Development</button>
+          <button className="nav-link active tab-text-color" id="Software-tab" data-bs-toggle="tab" data-bs-target="#Software" type="button" role="tab" aria-controls="Software" aria-selected="true">Software Development</button>
         </li>
         <li className="nav-item" role="presentation">
-          <button className="nav-link tab-text-color" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">UI Design & UX Research</button>
+          <button className="nav-link tab-text-color" id="Design-tab" data-bs-toggle="tab" data-bs-target="#Design" type="button" role="tab" aria-controls="Design" aria-selected="false">UI Design & UX Research</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link tab-text-color" id="UI-tab" data-bs-toggle="tab" data-bs-target="#UI" type="button" role="tab" aria-controls="UI" aria-selected="true">UI Examples</button>
         </li>
       </ul>
       <div className="tab-content" id="myTabContent">
-        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><SoftwareDetails/></div>
-        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><Design/></div>
+        <div className="tab-pane fade show active" id="Software" role="tabpanel" aria-labelledby="Software-tab"><SoftwareDetails/></div>
+        <div className="tab-pane fade" id="Design" role="tabpanel" aria-labelledby="Design-tab"><Design/></div>
       </div></div>
     </React.Fragment>
   );
